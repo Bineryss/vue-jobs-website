3f088c48aa28326148737435d3e265a6d8e3d9e2
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2021-10-02 16:14:47 +0200

    added api requests

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,18 +1,35 @@
 <template>
-	<h1>Job Details Page</h1>
-	<p>The job id is: {{ id }}</p>
+	<div v-if="job">
+		<h1>{{ job.title }}</h1>
+		<p>The job id is: {{ id }}</p>
+		<p>{{ job.description }}</p>
+	</div>
+	<div v-else>
+		<p>Loading job details...</p>
+	</div>
 </template>
 
 <script>
 	export default {
+		//ID von den rout params
 		props: ["id"],
 		// data() {
 		// 	return {
 		// 		id: this.$route.params.id,
 		// 	}
 		// },
+		data() {
+			return {
+				job: null,
+			}
+		},
+		mounted() {
+			fetch("http://localhost:3000/jobs/" + this.id)
+				.then((res) => res.json())
+				.then((data) => (this.job = data))
+				.catch((error) => console.log(error.message()))
+		},
 	}
 </script>
 
-<style>
-</style>
+<style></style>
